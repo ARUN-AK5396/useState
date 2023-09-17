5494e675d95754d6ed8a16ba54903e31bb1a680c
ARUN-AK5396 <jiarun333@gmail.com>
2023-09-17 19:40:15 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React , {useState} from 'react'
 import './App.css';
 
+
 function App() {
+
+  const [value,setValue] = useState(0);
+  const [user,setUser] = useState({name:'AK',age:1})
+
+  const increment = () => {
+      setValue(value+1)
+  }
+
+  const decrement = () => {
+    setValue(value-1)
+  }
+
+  const handleChange = (event) => {
+    const {name , value} = event.target;
+    setUser({...user,[name]:value});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{textAlign:'center'}}>Use State</h1>
+      <div style={{margin:'20px'}}>
+         <button onClick={decrement}>-</button>
+      </div>
+      <h2>Value : {value}</h2>
+      <div>
+        <button onClick={increment}>+</button>
+      </div> <br/><br/>
+
+      <div>
+        <input 
+          type='text'
+          name='name'
+          placeholder='name'
+          onChange={handleChange}
+          value={user.name}  
+        /><br/><br/>
+        <input 
+          type='number'
+          name='age'
+          value={user.age}
+          placeholder='age'
+          onChange={handleChange}
+        />
+        <h2>{user.name}, {user.age} </h2>
+      </div><br/>
     </div>
   );
 }
